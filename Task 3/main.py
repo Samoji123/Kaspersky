25e033e68922d3d849cb3b729705d6d7f80bf733
Samoji123 <samojlvich-a@school1409.ru>
2022-06-01 15:26:15 +0300

    did staff

---
+++ Task 3/main.py
@@ -0,0 +1,11 @@
+alf = 'qwertyuiopasdfghjklzxcvbnm_{}1234567890'
+text = 'fhke37_kdrjknbmpr_04374j'
+key = 'thekey'
+
+key = key * (len(text)//len(key)+1)
+
+ans = ''
+for i in range(len(text)):
+    ans += alf[(alf.index(text[i])+alf.index(key[i])+2)%len(alf)]
+    # ans += alf[(alf.index(text[i])+alf.index(key[i]))%len(alf)]
+print(ans)
