f737b608bad9f163c1acf51114b32042f978345c
Samoji123 <samojlvich-a@school1409.ru>
2022-06-01 15:00:52 +0300

    did staff

--- Task 2/main.py
+++ Task 2/main.py
@@ -1,4 +1,4 @@
 message = '23|8|1|20|4|15|25|15|21|11|14|15|23|1|2|15|21|20|1|2|3'.split('|')
 alf = ''.join([chr(i) for i in range(ord('a'), ord('z')+1)])
 ans = [alf[int(i)] for i in message]
-print(ans)
+print(''.join([str(i) for i in ans]))
