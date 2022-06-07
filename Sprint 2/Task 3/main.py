81ea9d52d1f11b8088fac220900b07fe968c067e
Samoji123 <samojlvich-a@school1409.ru>
2022-06-07 10:56:35 +0300

    did staff

--- Sprint 2/Task 3/main.py
+++ Sprint 2/Task 3/main.py
@@ -1,4 +1,4 @@
-with open('program.txt') as f:
+with open('program.txt', encoding = 'utf-8') as f:
     line = f.readline()
     d = [line.find('('), line.find(')')]
     line = line[d[0]+1:d[1]]
@@ -12,16 +12,16 @@ with open('program.txt') as f:
         elif '=' in line:
             vars.append(line[:line.find('=')].replace(' ', '')+' ')
 
-with open('program.txt') as f:
+with open('program.txt', encoding = 'utf-8') as f:
     text = f.read()
 
 array = list(set(array))
 vars = list(set(vars))
 
 for i in range(len(array)):
-    text.replace(array[i], 'R'+str(i+1))
+    text = text.replace(array[i], 'R'+str(i+1))
 for i in range(len(vars)):
-    text.replace(vars[i], 'a'+str(i+1))
+    text = text.replace(vars[i], 'a'+str(i+1))
 
-print(text,vars,array, sep = '\n')
+print(text)
 
