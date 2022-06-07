5124d01827f629610c94bfa4493ef0a8a9e64754
Smaji123 <samojilovich-a@school1409.ru>
2022-06-07 08:35:32 +0300

    did staff

---
+++ Sprint 2/Task 2/main.py
@@ -0,0 +1,11 @@
+with open('prog.txt', encoding='utf-8') as f:
+    text = f.read()
+    text = text[:2]
+    flag = False
+    if text == 'MZ':
+        flag = True
+
+if flag:
+    print('file is executable')
+else:
+    print('file is not executable')
---
+++ Sprint 2/Task 3/main.py
@@ -0,0 +1,26 @@
+with open('program.txt') as f:
+    line = f.readline()
+    d = [line.find('('), line.find(')')]
+    line = line[d[0]+1:d[1]]
+    vars = line.split(',')
+    array = []
+    for line in f.readlines():
+        if '+=' in line or '==' in line or '-=' in line or '*=' in line or '/=' in line or '**=' in line:
+            continue
+        elif '=[' in line:
+            array.append(line[:line.find('=')].replace(' ', '')+' ')
+        elif '=' in line:
+            vars.append(line[:line.find('=')].replace(' ', '')+' ')
+
+with open('program.txt') as f:
+    text = f.read()
+
+array = list(set(array))
+vars = list(set(vars))
+
+for i in range(len(array)):
+    text.replace(array[i], 'R'+str(i+1))
+for i in range(len(vars)):
+    text.replace(vars[i], 'a'+str(i+1))
+
+print(text,vars,array, sep = '\n')
